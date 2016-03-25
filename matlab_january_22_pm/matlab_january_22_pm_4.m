65928922508ff378c99d622c5b3a1e713680ff83
temach <tematibr@gmail.com>
2016-03-25 11:16:05 +0300

    Update matlab_january_22_pm_4.m

--- matlab_january_22_pm/matlab_january_22_pm_4.m
+++ matlab_january_22_pm/matlab_january_22_pm_4.m
@@ -2,12 +2,16 @@ n=22
 A = delsq(numgrid('S', n));
 spy(A)
 B = ones(length(A),1)
+spy(B)
 
-% choletsky incomplete (so we still have a sparse matrix)
+% choletsky incomplete (so we still have a sparse matrix) (preconditioner to help solve faster)
 R=ichol(A);
 
 figure(5);
-[x, flag, relres, iter,resvec] = pcg(A, b, 1e-6, 400);
+% relres =  also returns the relative residual norm(b-A*x)/norm(b)
+% iter = number fo iterations
+% resvec  = a vector of the residual norms at each iteration including norm(b-A*x0)
+[x, flag, relres, iter,resvec] = pcg(A,B, 1e-6, 400, R);
 semilogy(0:iter, resvec,'.b'); grid on;
 
 [x, flag, relres, iter, resvec]= pcg(A,B);
